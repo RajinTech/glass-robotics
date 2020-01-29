e1a20188affa589d36ccc7ed350afb4cfb0e7674
RajinTech <rajsglass@gmail.com>
2020-01-29 13:25:25 -0500

    removed readme

--- Saundra/motor.py
+++ Saundra/motor.py
@@ -4,7 +4,7 @@ GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BCM) # Use physical pin numbering
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 18 to be an input pin and set initial value to be pulled low (off)
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 17 to be an input pin and set initial value to be pulled low (off)
-
+#hello
 count = 0;
 while True: # Run forever
     print("start")
