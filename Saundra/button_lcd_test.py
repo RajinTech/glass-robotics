cd75d5ad02900c8c4ae1872810c8b1d3c1f094d5
RajinTech <rajsglass@gmail.com>
2020-01-29 16:24:48 -0500

    fixed names

--- Saundra/button_lcd_test.py
+++ Saundra/button_lcd_test.py
@@ -2,12 +2,9 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BCM) # Use physical pin numbering
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 18 to be an input pin and set initial value to be pulled low (off)
+
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 17 to be an input pin and set initial value to be pulled low (off)
-MODE = GPIO.input(17)
-PUSH = GPIO.input(18)
-#UP = GPIO.input(17)
-#DOWN = GPIO.input(17)
+
 
 
 #LCD SETUP
@@ -24,13 +21,7 @@ while True: # Run forever
     print(count)
     display.lcd_display_string(active_mode, 1)
     display.lcd_display_string(str(count), 2)
-    #PUSH
-    if GPIO.input(18) == True:
-        count = count + 1
-        print("Press Button was pushed!")
-        display.lcd_display_string("Press Button was Pushed", 1)
-        display.lcd_display_string(str(count), 2)
-        print(count)
+
     #TACTILE
     if GPIO.input(17) == False:
         count = count + 1
--- Saundra/button_test.py
+++ Saundra/button_test.py
@@ -2,7 +2,7 @@
 import RPi.GPIO as GPIO
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BCM) # Use physical pin numbering
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 18 to be an input pin and set initial value to be pulled low (off)
+
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 17 to be an input pin and set initial value to be pulled low (off)
 
 
@@ -10,11 +10,7 @@ count = 0;
 while True: # Run forever
     print("start")
     print(count)
-    #PUSH
-    if GPIO.input(18) == False:
-        count = count + 1
-        print("Press Button was pushed!")
-        print(count)
+
     #TACTILE
     if GPIO.input(17) == False:
         count = count + 1
