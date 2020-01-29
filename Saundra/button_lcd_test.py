51173f004dd480b57ba47f6dd64595ecd9ebacfb
RajinTech <rajsglass@gmail.com>
2020-01-29 14:03:53 -0500

    fixed names

--- Saundra/button_lcd_test.py
+++ Saundra/button_lcd_test.py
@@ -4,7 +4,7 @@ GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BCM) # Use physical pin numbering
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 18 to be an input pin and set initial value to be pulled low (off)
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 17 to be an input pin and set initial value to be pulled low (off)
-active_mode=""
+active_mode="Jog"
 MODE = GPIO.input(17)
 PUSH = GPIO.input(18)
 #UP = GPIO.input(17)
@@ -21,16 +21,19 @@ count = 0;
 while True: # Run forever
     print("start")
     print(count)
+    display.lcd_display_string(active_mode, 1)
+    display.lcd_display_string(count, 2)
     #PUSH
     if GPIO.input(18) == True:
         count = count + 1
         print("Press Button was pushed!")
         display.lcd_display_string("Press Button was Pushed", 1)
+        display.lcd_display_string(count, 2)
         print(count)
     #TACTILE
-    if GPIO.input(17) == False:
+    if MODE == False:
         count = count + 1
         print("Tactile Button was pushed!")
-        display.lcd_display_string("Tactile Button was Pushed", 1)
-
+        display.lcd_display_string("Tactile Pushed", 1)
+        display.lcd_display_string(count, 2)
         print(count)
