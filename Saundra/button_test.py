c22a8e1de7d86c3daa4ebe35ecb13aeaa4938118
RajinTech <rajsglass@gmail.com>
2020-01-29 13:57:41 -0500

    fixed names

--- Saundra/button_test.py
+++ Saundra/button_test.py
@@ -1,12 +1,9 @@
-#BUTTONs
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+#BUTTON SETUP
+import RPi.GPIO as GPIO
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BCM) # Use physical pin numbering
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 18 to be an input pin and set initial value to be pulled low (off)
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 17 to be an input pin and set initial value to be pulled low (off)
-
-
-
 
 
 count = 0;
--- Saundra/run_saw.py
+++ Saundra/run_saw.py
@@ -4,9 +4,17 @@ GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BCM) # Use physical pin numbering
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 18 to be an input pin and set initial value to be pulled low (off)
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 17 to be an input pin and set initial value to be pulled low (off)
+active_mode=""
+MODE = GPIO.input(17)
+PUSH = GPIO.input(18)
+#UP = GPIO.input(17)
+#DOWN = GPIO.input(17)
 
 
-
+#LCD SETUP
+import lcddriver
+import time
+display = lcddriver.lcd()
 
 
 count = 0;
@@ -17,9 +25,12 @@ while True: # Run forever
     if GPIO.input(18) == False:
         count = count + 1
         print("Press Button was pushed!")
+        display.lcd_display_string("Press Button was Pushed", 1)
         print(count)
     #TACTILE
     if GPIO.input(17) == False:
         count = count + 1
         print("Tactile Button was pushed!")
+        display.lcd_display_string("Tactile Button was Pushed", 1)
+
         print(count)
