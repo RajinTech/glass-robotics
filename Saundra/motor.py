a3c56390fdd8341580a200d0c9eb789088910b68
RajinTech <rajsglass@gmail.com>
2020-01-28 23:19:23 -0500

    saundra

---
+++ Saundra/motor.py
@@ -0,0 +1,17 @@
+#PRESS BUTTON
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BCM) # Use physical pin numbering
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 18 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 17 to be an input pin and set initial value to be pulled low (off)
+
+count = 0;
+while True: # Run forever
+    if GPIO.input(18) == GPIO.HIGH:
+        count ++
+        print("Press Button was pushed!")
+        print(count)
+    if GPIO.input(17) == False:
+        count ++
+        print("Tactile Button was pushed!")
+        print(count)
