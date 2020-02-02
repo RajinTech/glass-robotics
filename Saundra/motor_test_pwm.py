92d285d65a5b628c0a9b030e6f306cdff6e60786
RajinTech <rajsglass@gmail.com>
2020-02-02 18:18:18 -0500

    update repo

--- Saundra/motor_test_pwm.py
+++ Saundra/motor_test_pwm.py
@@ -2,11 +2,11 @@ from time import sleep
 import RPi.GPIO as GPIO
 from gpiozero import PWMLED
 
+GPIO.setmode(GPIO.BCM)
 TRACK_STEP = PWMLED(20)  # Step GPIO Pin
 TRACK_DIR = PWMLED(21)   # Direction GPIO Pin
 CW = 1     # Clockwise Rotation
 CCW = 0    # Counterclockwise Rotation
-
 sleep_count = 1
 
 TRACK_DIR.value = CCW
