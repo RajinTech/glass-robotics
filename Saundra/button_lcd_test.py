99ee478873bd347a6c22bdda551ba5bacaa0d6ef
RajinTech <rajsglass@gmail.com>
2020-02-02 19:55:28 -0500

    fixed profile dropdown

--- Saundra/button_lcd_test.py
+++ Saundra/button_lcd_test.py
@@ -3,6 +3,8 @@ import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BCM) # Use physical pin numbering
 
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 17 to be an input pin and set initial value to be pulled low (off)
 
 
@@ -21,11 +23,28 @@ while True: # Run forever
     print("start")
     print(count)
 
-
     #TACTILE
     if GPIO.input(17) == False:
-        print("Tactile Button was pushed!")
-        display.lcd_display_string(active_mode + " Tactile Pushed", 1)
+        print("Mode Button was pushed!")
+        display.lcd_display_string(active_mode + " Mode Pushed", 1)
+        display.lcd_display_string(str(count), 2)
+        count = count + 1
+        speed_timer = speed_timer + 1
+        print(count)
+        if speed_timer < 7:
+            time.sleep(1)
+    else if GPIO.input(23) == False:
+        print("Down Button was pushed!")
+        display.lcd_display_string(active_mode + " Down Pushed", 1)
+        display.lcd_display_string(str(count), 2)
+        count = count + 1
+        speed_timer = speed_timer + 1
+        print(count)
+        if speed_timer < 7:
+            time.sleep(1)
+    else if GPIO.input(24) == False:
+        print("Up Button was pushed!")
+        display.lcd_display_string(active_mode + " Up Pushed", 1)
         display.lcd_display_string(str(count), 2)
         count = count + 1
         speed_timer = speed_timer + 1
