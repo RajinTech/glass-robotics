6f1e68fe915da984f601256d77136d0d11183b6d
RajinTech <rajsglass@gmail.com>
2020-02-02 21:33:34 -0500

    typo

--- Saundra/run_saundra.py
+++ Saundra/run_saundra.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 from gpiozero import LED
-from time import sleep
+from time import sleep 
 GPIO.setmode(GPIO.BCM) # Use physical pin numbering
 GPIO.setwarnings(False) # Ignore warning for now
 
