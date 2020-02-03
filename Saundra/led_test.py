0613a664382238e82ad995445b47ed4bef2be146
RajinTech <rajsglass@gmail.com>
2020-02-02 22:02:12 -0500

    typo

---
+++ Saundra/led_test.py
@@ -0,0 +1,39 @@
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+from gpiozero import LED
+from time import sleep
+#LEDS
+white = LED(06)
+red = LED(13)
+yellow = LED(19)
+green = LED(26)
+
+white.value = 1
+red.value = 1
+yellow.value = 1
+green.value = 1
+sleep(1)
+white.value = 0
+red.value = 0
+yellow.value = 0
+green.value = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
--- Saundra/run_saundra.py
+++ Saundra/run_saundra.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 from gpiozero import LED
-from time import sleep 
+from time import sleep
 GPIO.setmode(GPIO.BCM) # Use physical pin numbering
 GPIO.setwarnings(False) # Ignore warning for now
 
@@ -10,7 +10,13 @@ GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 17 to be an input pin and set initial value to be pulled low (off)
 
 #RELAY
-GPIO.setup(25, GPIO.OUT)
+RELAY = LED(25)
+
+#LEDS
+WHITE = LED(06)
+RED = LED(13)
+YELLOW = LED(19)
+GREEN = LED(26)
 
 #MOTOR
 TRACK_STEP = LED(20)  # Step GPIO Pin
