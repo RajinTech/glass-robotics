2dd272fe19e3b558fc62bd443aeb588a46bad610
RajinTech <rajsglass@gmail.com>
2020-02-02 20:11:53 -0500

    fixed profile dropdown

--- Saundra/button_lcd_test.py
+++ Saundra/button_lcd_test.py
@@ -15,46 +15,47 @@ import time
 display = lcddriver.lcd()
 
 #STATE
-active_mode="Jog"
-speed_timer = 0;
-count = 0;
+modes = ["Distance", "Speed", "Move", "Ready?", "Running"]
+active_mode = "Distance"
+speed_timer = 0
+count = 0
 print("start")
 print(count)
 
 while True: # Run forever
-
-    #TACTILE
-    if GPIO.input(17) == False:
-        print("Mode Button was pushed!")
-        display.lcd_display_string(active_mode + " Mode Pushed", 1)
-        display.lcd_display_string(str(count), 2)
-        count = count + 1
-        speed_timer = speed_timer + 1
-        print(count)
-        if speed_timer < 7:
-            time.sleep(1)
-    elif GPIO.input(23) == False:
-        print("Down Button was pushed!")
-        display.lcd_display_string(active_mode + " Down Pushed", 1)
-        display.lcd_display_string(str(count), 2)
-        count = count + 1
-        speed_timer = speed_timer + 1
-        print(count)
-        if speed_timer < 7:
-            time.sleep(1)
-    elif GPIO.input(24) == False:
-        print("Up Button was pushed!")
-        display.lcd_display_string(active_mode + " Up Pushed", 1)
-        display.lcd_display_string(str(count), 2)
-        count = count + 1
-        speed_timer = speed_timer + 1
-        print(count)
-        if speed_timer < 7:
-            time.sleep(1)
-    else:
-        speed_timer = 0
-        display.lcd_display_string(active_mode, 1)
-        display.lcd_display_string(str(count), 2)
+    while active_mode == "Distance":
+        #TACTILE
+        if GPIO.input(17) == False:
+            print("Mode Button was pushed!")
+            display.lcd_display_string(active_mode + " Mode Pushed", 1)
+            display.lcd_display_string(str(count), 2)
+            count = count + 1
+            speed_timer = speed_timer + 1
+            print(count)
+            if speed_timer < 7:
+                time.sleep(1)
+        elif GPIO.input(23) == False:
+            print("Down Button was pushed!")
+            display.lcd_display_string(active_mode + " Down Pushed", 1)
+            display.lcd_display_string(str(count), 2)
+            count = count - 1
+            speed_timer = speed_timer + 1
+            print(count)
+            if speed_timer < 7:
+                time.sleep(1)
+        elif GPIO.input(24) == False:
+            print("Up Button was pushed!")
+            display.lcd_display_string(active_mode + " Up Pushed", 1)
+            display.lcd_display_string(str(count), 2)
+            count = count + 1
+            speed_timer = speed_timer + 1
+            print(count)
+            if speed_timer < 7:
+                time.sleep(1)
+        else:
+            speed_timer = 0
+            display.lcd_display_string(active_mode, 1)
+            display.lcd_display_string(str(count), 2)
 
 
 
